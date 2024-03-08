07df89bd61418b17890e04578fe6a48ece568dc8
mohammad <mohammadsalimi04@gmail.com>
2024-03-09 00:39:17 +0330

    fix show previouse week

--- app/Http/Controllers/API/SearchControllerApi.php
+++ app/Http/Controllers/API/SearchControllerApi.php
@@ -79,23 +79,6 @@ class SearchControllerApi extends BaseController
         
          
         }
-        public function showPreviouseWeek(Request $request){
-            $service_id=$request->service_id;
-            $startTime=$request->startTime;
-            $date=parent::exploadDateFormat($startTime);
-            $date=(new Jalalian($date[0],$date[1],$date[2]));
-             $days_of_week=$this->getPreviouseWeek($service_id,$date,$startTime);
-                $days=$days_of_week[0];
-                $sunday=$days_of_week[1]->format('Y/m/d');
-                $stop=$days_of_week[2];
-                $friday=$date->subDays(1)->format('Y/m/d');
-                $days=array_reverse($days);
-                return response()->json([$days,$friday,$sunday,$stop],200);
-         
-               
-                
-               
-        }
         public function getNextWeek($service_id,$currentTime,$currentTimeFormat){
             $date=parent::exploadDateFormat($currentTimeFormat);
             $endTime=(new Jalalian($date[0],$date[1],$date[2]))->addDays(7);
@@ -117,6 +100,24 @@ class SearchControllerApi extends BaseController
             }
                 return [$days,$endTime];
             }
+        public function showPreviouseWeek(Request $request){
+            $service_id=$request->service_id;
+            $startTime=$request->startTime;
+            $date=parent::exploadDateFormat($startTime);
+            $date=(new Jalalian($date[0],$date[1],$date[2]));
+             $days_of_week=$this->getPreviouseWeek($service_id,$date,$startTime);
+                $days=$days_of_week[0];
+                $sunday=$days_of_week[1]->format('Y/m/d');
+                $stop=$days_of_week[2];
+                $friday=$date->subDays(1)->format('Y/m/d');
+                $days=array_reverse($days);
+                return response()->json([$days,$friday,$sunday,$stop],200);
+         
+               
+                
+               
+        }
+       
             public function getPreviouseWeek($service_id,$currentTime,$currentTimeFormat){
                 $date=parent::exploadDateFormat($currentTimeFormat);
                 $startTime=(new Jalalian($date[0],$date[1],$date[2]))->subDays(7);
